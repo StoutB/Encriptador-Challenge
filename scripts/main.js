478e206b7f026b15099b5049d9982167d57c59b1
Fausto Uriarte <faustouriweb@gmail.com>
2022-08-15 17:46:19 -0300

    Funcion deteccion mayus y mejor uso de variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,31 +1,38 @@
 //elementos
 
-var resultado = document.getElementById("resultado");
-var input = document.getElementById("usertext");
-var encriptarBtn = document.getElementById('encriptar');
-var desencriptarBtn = document.getElementById('desencriptar');
-var copiarBtn = document.getElementById('copiar');
-var muneco = document.querySelector('.muneco');
+const resultado = document.getElementById("resultado");
+const input = document.getElementById("usertext");
+const encriptarBtn = document.getElementById('encriptar');
+const desencriptarBtn = document.getElementById('desencriptar');
+const copiarBtn = document.getElementById('copiar');
+const muneco = document.querySelector('.muneco');
+const letras = /^[a-z0-9 ]*$/;
 
 copiarBtn.style.display = "none";
 //funciones
 
 function encriptar() {
-    var userMsj = input.value;    
-    var encryptedMsj = userMsj
-    .replace(/e/g, "enter")
-    .replace(/i/g, "imes")
-    .replace(/a/g, "ai")
-    .replace(/ober/g, "o")
-    .replace(/u/g, "u");
-    resultado.value = encryptedMsj;
-    muneco.style.display = "none";
-    copiarBtn.style.display = "inline";
+    if (!letras.test(input.value) || input.value.length === 0)
+    {
+        alert('Solo se permite el uso de minusculas');
+    }
+    else{
+        let userMsj = input.value;    
+        let encryptedMsj = userMsj
+        .replace(/e/g, "enter")
+        .replace(/i/g, "imes")
+        .replace(/a/g, "ai")
+        .replace(/ober/g, "o")
+        .replace(/u/g, "u");
+        resultado.value = encryptedMsj;
+        muneco.style.display = "none";
+        copiarBtn.style.display = "inline";
+    }
 }
 
 function desencriptar() {
-    var userMsj = input.value;    
-    var desencryptedMsj = userMsj
+    let userMsj = input.value;    
+    let desencryptedMsj = userMsj
     .replace(/enter/g, "e")
     .replace(/imes/g, "i")
     .replace(/ai/g, "a")
@@ -34,15 +41,27 @@ function desencriptar() {
     resultado.value = desencryptedMsj;
 }
 
-function borrarMuneco(){
-    muneco.isConnected;
-    muneco.remove;
-    muneco.isConnected;
-}
 
 function copiar() {
     navigator.clipboard.writeText(resultado.value);
     alert("Texto copiado");
+}
+
+function validar() {
+    let frase = input.value; 
+    for(var i = 0; i < frase.length; i++)
+{
+    var letraActual = miPalabra.charAt(index);
+    if(esMayuscula(letraActual))
+    {
+        alert("La letra " + letraActual + " es mayúscula");
+    }
+
+    if(esMinuscula(letraActual))
+    {
+        alert("La letra " + letraActual + " es minúscula");
+    }       
+}
 }
 
 //botones
